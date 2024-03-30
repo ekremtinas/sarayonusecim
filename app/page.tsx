e0231a5fe4d722240ab1d7c8e1c6590525375edf
ekremtinas <ekremtinas@gmail.com>
2024-03-30 05:19:39 +0300

    son hali

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,148 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore,collection,getDocs,addDoc,doc,updateDoc} from "@firebase/firestore"
+import { deleteDoc } from "firebase/firestore";
+
+
 
 export default function Home() {
+
+  const path = !!document ? document?.location?.href : !!window ? window?.location?.href : "";
+  console.log(path)
+  const [isAdmin, setIsAdmin] = useState(path?.includes("key=Mt1453.!"));
+  console.log(isAdmin)
+  const firebaseConfig = {
+    apiKey: "AIzaSyDQbf3tbDFUS6nvc84o116PN9MIWe4r7Tk",
+    authDomain: "muhtarliksecim.firebaseapp.com",
+    projectId: "muhtarliksecim",
+    storageBucket: "muhtarliksecim.appspot.com",
+    messagingSenderId: "774918961056",
+    appId: "1:774918961056:web:e4ecb45d59e46a1bf2d6c5",
+    measurementId: "G-F90C942RRY"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const db = getFirestore(app);
+
+  const CollectionRef = collection(db, "muhtarliksecim")
+
+
+
+  const getUsersData = async () => {
+    const data = await getDocs(CollectionRef)
+    setSandiklar(data.docs.map((elem:any) => ({ ...elem.data(), id: elem.id })))
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+    getUsersData()
+    }, 1000);
+}, [])
+
+
+  const [sorumlu, setSorumlu] = useState("");
+  const [sandikNo, setSandikNo] = useState("");
+  const [oySayisi, setOySayisi] = useState(0);
+  const [sandiklar, setSandiklar] = useState<any>([]);
+
+const update =async(id:any,oySayisi:any)=>{
+  const sandikDoc = doc(db, "muhtarliksecim", id);
+  console.log(sandikDoc)
+  const newReg = { oySayisi}
+  console.log("Updated the Data on System")
+  await updateDoc(sandikDoc, newReg)
+  console.log("Updated the Data on the Server")
+}
+
+  const oySayisiGuncelle = (id:any, oySayisi:any) => {
+    setSandiklar(sandiklar.map((sandik:any) => {
+      if (sandik.id === id) {
+        update(id,oySayisi);
+
+
+        return { ...sandik, oySayisi };
+
+
+        
+      } else {
+        return sandik;
+      }
+    }));
+  };
+
+
+  const sandikEkle =async()=>{
+    setSandiklar([...sandiklar, { sandikNo, sorumlu, oySayisi }]);
+    await addDoc(CollectionRef, { sandikNo, sorumlu, oySayisi });
+    setSorumlu("");
+    setSandikNo("");
+    setOySayisi(0);
+    
+  }
+  const sandikSil =async(id:any)=>{
+    setSandiklar(sandiklar.filter((s:any) => s.id !==id));
+    const sandikDoc = doc(db, "muhtarliksecim", id);
+    console.log(sandikDoc)
+    await deleteDoc(sandikDoc)
+    console.log("Updated the Data on the Server")
+  }
+  console.log(sandiklar)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="flex  flex-col items-center justify-between p-24">
+       
+        <h2 style={{textAlign:"center",fontSize:20}}><b>MUSTAFA TINAS</b></h2>
+        <br></br>
+        <h2 style={{textAlign:"center",fontSize:20}}><b>Ladik Mahallesi Seçim Sandık Kayıtları</b></h2>
+        <br></br>
+        <table>
+        <thead>
+          <tr>
+            <th>Sorumlu</th>
+            <th>Sandık No</th>
+            <th>Oy Sayısı</th>
+            <th>İşlem</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sandiklar.map((sandik:any) => (
+            <tr key={sandik.sandikNo}>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              <td>{sandik.sorumlu}</td>
+              <td>{sandik.sandikNo}</td>
+              <td style={{textAlign:"justify"}}>
+                <input readOnly={!isAdmin} type="number" value={sandik.oySayisi} onChange={e => oySayisiGuncelle(sandik.id, parseInt(e.target.value))} />
+                
+              </td>
+              <td>
+              <button hidden={!isAdmin} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => {
+                 sandikSil(sandik.id);
+                }}>Sil</button>
+              </td>
+            </tr>
+          ))}
+          <tr hidden={!isAdmin}>
+            <td  >
+              <input type="text" value={sorumlu} onChange={(e:any)=>setSorumlu(String(e.target.value))} placeholder="Sorumlu" />
+              </td>
+              <td>
+              <input type="text" value={sandikNo} onChange={(e:any)=>setSandikNo(String(e.target.value))}   placeholder="Sandık No" />
+              </td>
+              <td>
+              <input type="number"  value={oySayisi} onChange={(e:any)=>setOySayisi(Number(e.target.value))} placeholder="Oy Sayısı" />
+              </td>
+              <td>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={sandikEkle}>Ekle</button>
+                </td>
+             
+          </tr>
+        </tbody>
+      </table>
+      
     </main>
   );
 }
